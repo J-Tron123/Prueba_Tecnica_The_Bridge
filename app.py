5e31eabd7cf313e7b1404b287e20c3a5452b6694
Juan José <correopersonal436@gmail.com>
2022-05-24 02:49:17 +0200

    Method app

--- app.py
+++ app.py
@@ -4,24 +4,31 @@ from utils.models import Clean
 
 app = Flask(__name__)
 
-@app.route("/api/v1/resources/books/all", methods=["GET"])
+@app.route("/api/v1/resources/books/all", methods=["GET", "POST"])
 def get_all():
-    with open("data/finished_model.model", "rb") as file:
-        model = pickle.load(file)
+    if request.method == "GET":
 
-    text = request.args.get("text")
-    text = Clean().clean_emojis(text)
-    text = Clean().remove_links(text)
-    text = Clean().signs_tweets(text)
+        return {"Instructions": """Use the following endpoint '/api/v1/resources/books/all' 
+        with the param 'text' followed by a text to predict"""}
 
-    prediction = model.predict(text)
+    else:
 
-    if prediction == 0:
-        result = "Positive sentiment"
-    else: 
-        result = "Negative sentiment"
+        with open("data/finished_model.model", "rb") as file:
+            model = pickle.load(file)
 
-    return {"Prediction": result}
+        text = request.args.get("text")
+        text = Clean().clean_emojis(text)
+        text = Clean().remove_links(text)
+        text = Clean().signs_tweets(text)
+
+        prediction = model.predict(text)
+
+        if prediction == 0:
+            result = "Positive sentiment"
+        else: 
+            result = "Negative sentiment"
+
+        return {"Prediction": result}
 
 
-# app.run()
+app.run()
